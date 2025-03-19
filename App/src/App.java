da64393d65f5a69c70607fbaf10328d208c90bba
32338 <142840950+32338@users.noreply.github.com>
2025-03-19 23:59:44 +0000

    aa

--- App/src/App.java
+++ App/src/App.java
@@ -1,9 +1,10 @@
 import java.sql.*;
+import java.math.BigDecimal;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boa noite!");
-        String url = "jdbc:postgresql://db.mlfmvbhmbmirmmrhfzsv.supabase.co:5432/postgres?user=postgres&password=Tresdfghjm,lkjh";
+        String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:6543/postgres?user=postgres.mlfmvbhmbmirmmrhfzsv&password=Tresdfghjm,lkjh";
         Connection conexao = DriverManager.getConnection(url);
         String sql = "SELECT * FROM contas";
         PreparedStatement stm = conexao.prepareStatement(sql);
@@ -14,5 +15,14 @@ public class App {
             System.out.print("Número da conta: " + nroConta);
             System.out.println(" tem saldo de R$ " + saldo);
         }
+        create();
+    }
+    public static void create() throws SQLException{
+        System.out.print("Numero para a nova conta: ");
+        long nro = Long.parseLong(System.console().readLine());
+        System.out.print("Saldo da nova conta: ");
+        BigDecimal saldo = new BigDecimal(System.console().readLine();
+        
+
     }
 }
